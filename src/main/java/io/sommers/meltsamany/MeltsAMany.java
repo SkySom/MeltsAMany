002602dee62cfa2e38afe8bcbf3231c836ddfbe2
Skylar Sommers <ssommers@criticalmix.com>
2017-04-27 16:14:39 -0400

    Ran create project

---
+++ src/main/java/io/sommers/meltsamany/MeltsAMany.java
@@ -0,0 +1,18 @@
+package io.sommers.meltsamany;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = MeltsAMany.MOD_ID, name = MeltsAMany.MOD_NAME, version = MeltsAMany.VERSION)
+public class MeltsAMany {
+
+    public static final String MOD_ID = "meltaamany";
+    public static final String MOD_NAME = "MeltsAMany";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
