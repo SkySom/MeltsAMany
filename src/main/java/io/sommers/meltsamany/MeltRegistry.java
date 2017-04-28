7fcfd7edb96315306a8263e0c5f515e0087f9844
Skylar Sommers <ssommers@criticalmix.com>
2017-04-28 10:27:11 -0400

    Changed Register method to use ILiquidStack

--- src/main/java/io/sommers/meltsamany/MeltRegistry.java
+++ src/main/java/io/sommers/meltsamany/MeltRegistry.java
@@ -1,9 +1,9 @@
 package io.sommers.meltsamany;
 
+import minetweaker.MineTweakerAPI;
 import minetweaker.api.item.IItemStack;
+import minetweaker.api.liquid.ILiquidStack;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
@@ -11,13 +11,24 @@ import stanhebben.zenscript.annotations.ZenMethod;
 @ZenClass("mods.meltsamany.MeltRegistry")
 public class MeltRegistry {
     @ZenMethod
-    public static void addSecondaryResult(IItemStack itemStackInput, String outputFluidName, int outputAmount) {
-        Fluid fluid = FluidRegistry.getFluid(outputFluidName);
-        if (fluid != null) {
-            FluidStack fluidStack = new FluidStack(fluid, outputAmount);
-            if (itemStackInput.getInternal() instanceof ItemStack) {
-                MeltsAMany.instance.meltEntryList.add(new MeltEntry((ItemStack) itemStackInput.getInternal(), fluidStack));
-            }
+    public static void addSecondaryResult(IItemStack itemStackInput, ILiquidStack liquidStack) {
+        ItemStack inputStack = null;
+        FluidStack fluidStack = null;
+
+        if (itemStackInput != null && itemStackInput.getInternal() instanceof ItemStack) {
+            inputStack = (ItemStack) itemStackInput.getInternal();
+        } else {
+            MineTweakerAPI.getLogger().logError("Couldn't find ItemStack input");
+        }
+
+        if (liquidStack != null && liquidStack.getInternal() instanceof FluidStack) {
+            fluidStack = (FluidStack) liquidStack.getInternal();
+        } else {
+            MineTweakerAPI.getLogger().logError("Couldn't find FluidStack output");
+        }
+
+        if (fluidStack != null && inputStack != null) {
+            MeltsAMany.instance.meltEntryList.add(new MeltEntry(inputStack, fluidStack));
         }
     }
 }
