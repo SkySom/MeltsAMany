ea89ed281c677ac5023666e510e07eff0d09286a
Skylar Sommers <ssommers@criticalmix.com>
2017-04-28 10:27:35 -0400

    MeltHandler now better accounts for potential full smelteries

--- src/main/java/io/sommers/meltsamany/MeltHandler.java
+++ src/main/java/io/sommers/meltsamany/MeltHandler.java
@@ -2,16 +2,24 @@ package io.sommers.meltsamany;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import slimeknights.tconstruct.library.smeltery.SmelteryTank;
 import slimeknights.tconstruct.smeltery.events.TinkerSmelteryEvent;
 
 public class MeltHandler {
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onMelting(TinkerSmelteryEvent.OnMelting onMeltingEvent) {
         for (MeltEntry meltEntry : MeltsAMany.instance.meltEntryList) {
             if (ItemStack.areItemStacksEqual(meltEntry.getItemStack(), onMeltingEvent.itemStack)) {
                 FluidStack additional = meltEntry.getFluidStack().copy();
-                onMeltingEvent.smeltery.getTank().fill(additional, true);
+                SmelteryTank tank = onMeltingEvent.smeltery.getTank();
+                int currentAvailable = tank.getCapacity() - tank.getFluidAmount();
+                currentAvailable -= onMeltingEvent.result.amount;
+                if (additional.amount > currentAvailable) {
+                    additional.amount = currentAvailable;
+                }
+                tank.fill(additional, true);
             }
         }
     }
