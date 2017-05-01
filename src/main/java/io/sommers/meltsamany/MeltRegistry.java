826e52a83e1edacb2a147058ddb6acb79f821b11
Skylar Sommers <ssommers@criticalmix.com>
2017-04-30 21:34:58 -0400

    Should work with OreDict now too

--- src/main/java/io/sommers/meltsamany/MeltRegistry.java
+++ src/main/java/io/sommers/meltsamany/MeltRegistry.java
@@ -1,24 +1,35 @@
 package io.sommers.meltsamany;
 
+import com.google.common.collect.Lists;
 import minetweaker.MineTweakerAPI;
+import minetweaker.api.item.IIngredient;
 import minetweaker.api.item.IItemStack;
 import minetweaker.api.liquid.ILiquidStack;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
+import java.util.List;
+
 @ZenClass("mods.meltsamany.MeltRegistry")
 public class MeltRegistry {
     @ZenMethod
-    public static void addSecondaryResult(IItemStack itemStackInput, ILiquidStack... liquidStacks) {
-        ItemStack inputStack = null;
+    public static void addSecondaryResult(IIngredient itemStackInput, ILiquidStack... liquidStacks) {
+        List<ItemStack> inputStack = null;
         FluidStack[] fluidStacks = null;
 
-        if (itemStackInput != null && itemStackInput.getInternal() instanceof ItemStack) {
-            inputStack = (ItemStack) itemStackInput.getInternal();
+
+        if (itemStackInput != null) {
+            inputStack = Lists.newArrayList();
+            for (IItemStack itemStack : itemStackInput.getItems()) {
+                if (itemStack.getInternal() instanceof ItemStack) {
+                    inputStack.add((ItemStack) itemStack.getInternal());
+                }
+            }
         } else {
-            MineTweakerAPI.getLogger().logError("Couldn't find ItemStack input");
+            MineTweakerAPI.getLogger().logError("You must enter an IIngredient");
         }
 
         if (liquidStacks != null) {
@@ -35,9 +46,11 @@ public class MeltRegistry {
             MineTweakerAPI.getLogger().logError("You must enter at least one ILiquidStack");
 
         }
-        
+
         if (fluidStacks != null && inputStack != null) {
-            MeltsAMany.instance.getMeltEntries().put(inputStack.toString(), fluidStacks);
+            for (ItemStack itemStack : inputStack) {
+                MeltsAMany.instance.getMeltEntries().put(itemStack.toString(), fluidStacks);
+            }
         }
     }
 }
